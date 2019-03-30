302e4c45fc84d5ac7e545b05ad1b4c140463a4e3
Aditya Chavan <adityachavan198@gmail.com>
2019-03-30 16:19:04 +0530

    new-project
    renewed project

--- MoneyMe/Money/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-from django.conf.urls import url
-urlpatterns=[
-    # path('', views.index, name='index'),
-    path('summary/', views.summary, name='Summary')
-
-]
