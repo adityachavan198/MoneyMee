56288bf082b471f1bc6534cd437616144bff6173
Aditya Chavan <adityachavan198@gmail.com>
2019-03-30 17:17:32 +0530

    urls

--- MoneyMee/MoneyMe/Money/views.py
+++ MoneyMee/MoneyMe/Money/views.py
@@ -22,8 +22,29 @@ def index(request):
     return render(request, "Money/index.html")
 
 def showregister(request):
-    return redirect(request,'library/register.html')
+    return redirect(request,'Money/register.html')
 
 
+def registeruser(request):
+    device = request.POST['device']
+    email = request.POST['email']
+    password = request.POST['password']
+    first_name = request.POST['fname']
+    last_name = request.POST['lname']
+    phone = request.POST['phone']
+    user = UserOfApp.objects.create_user(email=email,password=password,first_name=first_name, last_name=last_name,phone=phone)
+    user.save()
+    if device == 'web':
+        render(request,'Money/login.html')
+    else:
+        return HttpResponse(json.dumps(["True"]), content_type='application/json')
 
-    
+def authenticate(request):
+    username = request.POST['username']
+    password = request.POST['password']
+    l = Student.objects.filter(username=username, password=password)
+    if len(l):
+        request.session['username'] = username
+        return HttpResponseRedirect(reverse('showhomepage'))
+    else:
+        return HttpResponseRedirect(reverse('libraryindex'))
